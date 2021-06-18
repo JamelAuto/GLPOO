a95af04a258be191cfd345b4da16cba7deb1ade3
Jimmy Moren <jimmy.mro@outlook.fr>
2021-06-18 10:53:23 +0200

    Implement Maven and jUnit

---
+++ src/test/java/musichub/business/SongTest.java
@@ -0,0 +1,12 @@
+package musichub.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SongTest {
+
+    @Test
+    void getGenre() {
+    }
+}
